819dee7182d04c1573037fee4ac6cf9723c012ab
zadereymaria <maksjoystick@gmail.com>
2022-04-07 22:41:23 +0300

    first class

--- index2.php
+++ index2.php
@@ -1,1 +1,35 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+//$email = 'some@gmail.com';
+//var_dump(filter_var($email,FILTER_VALIDATE_EMAIL));
+
+class Email
+{
+    private $email;
+
+
+    public function  __construct($email)
+    {
+        if (!filter_var($email,FILTER_VALIDATE_EMAIL))
+        {
+            throw new InvalidArgumentException($email);
+        }
+        $this->email = $email;
+    }
+
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
+$email1 = new Email('email1@gmail.com');
+$email2 = new Email('email2@gmail.com');
+
+
+    var_dump($email1->getEmail(), $email2->getEmail());
+
+
