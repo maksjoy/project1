0d07dfdffa62f18b0778f586d06754f441e07bd6
zadereymaria <maksjoystick@gmail.com>
2022-04-07 22:01:11 +0300

    commit 5

--- index.php
+++ index.php
@@ -1,2 +1,42 @@
 <?php
-phpinfo();
+$g = 40;
+$dt = new DateTime();
+// int null float string object
+class Example
+{
+    public $a;
+
+    public static $b;
+
+    public function  __construct($a)
+    {
+        $this->a = $a;
+    }
+
+
+    public static function getA($a)
+    {
+
+    }
+
+
+    public static function setB($b)
+    {
+
+    }
+}
+Example::setB(10);
+
+$ex1 = new Example(10);
+
+$ex1::$b = 30;
+
+$ex2 = new Example(20);
+
+$ex2::$b = 40;
+
+$ex3 = new Example(100);
+
+echo $ex3::$b;
+
+
